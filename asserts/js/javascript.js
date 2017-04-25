3dc8ffb1dd271a8f9afb3821c1c7aefe981b69df
marcecabelloherrera <marcecabello.h@gmail.com>
2017-04-24 21:33:04 -0300

    ejercicio extra aún no está listo

---
+++ asserts/js/javascript.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("farenheit").value;
+
+
+var grados = [];//temperatura en celcius que queremos convertir a farenheit
+var convertirCelcius= grados.map (function(elemento){//función que convertira celcius en farenheit 
+        return((elemento*(9/5))+32) //fórmula matemática que convierte grados celcius en farenheit             
+        });
+document.write(celcius);
+
